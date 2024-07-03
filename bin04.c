8a79cc48f8f7f1a756a7be09f241b340cd89b623
iambin6 <miraisan2512@gmail.com>
2024-07-03 13:28:33 +0700

    Update bin04.c

--- bin04.c
+++ bin04.c
@@ -1,26 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-
 int main() {
   char s[101];
   char output[101] = "";
   int i, j = 0;
-
   printf("Enter: s = ");
   fgets(s, sizeof(s), stdin);
-
-  // Xóa ký t? newline n?u có
   s[strcspn(s, "\n")] = '\0';
-
   for (i = 0; s[i] != '\0'; i++) {
     if (isalpha(s[i]) || isspace(s[i])) {
       output[j] = s[i];
       j++;
     }
   }
-  output[j] = '\0'; // Null-terminate the output string
-
+  output[j] = '\0'; 
   printf("OUTPUT:\n%s\n", output);
 
   return 0;
